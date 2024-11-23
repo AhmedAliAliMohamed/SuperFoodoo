8c70cd06798450f2315e04d7faa13fc324f07971
Ahmed <ahmedalimohammedgebril@gmail.com>
2024-11-23 14:41:21 +0200

    Change ui in fragments

---
+++ app/src/main/java/com/example/kotlinappfood/utilities/CharSequenceExtensions.kt
@@ -0,0 +1,36 @@
+package com.example.kotlinappfood.utilities
+
+import android.util.Patterns
+import java.util.regex.Pattern
+
+fun CharSequence.isPassword(): Boolean {
+    return matches(Regex("^.{8,}\$"))
+}
+
+fun CharSequence.limit(min: Int, max: Int): Boolean {
+    return matches(Regex("^.{$min,$max}\$"))
+}
+
+fun CharSequence.isEmail(): Boolean {
+    return matches(Patterns.EMAIL_ADDRESS.toRegex())
+}
+
+fun CharSequence.isPhone(): Boolean {
+    return matches(Pattern.compile("^[0-9]{10}$").toRegex())
+}
+
+fun CharSequence.isDigits(): Boolean {
+    return matches(Regex("^[0-9]*\$"))
+}
+
+fun CharSequence.isUserName(): Boolean {
+    return matches(Pattern.compile("^[a-zA-Z]+$").toRegex())
+}
+
+fun CharSequence.isFullName(): Boolean {
+    return matches(Pattern.compile("^[A-Za-z][A-Za-z\\s]+$").toRegex())
+}
+
+fun CharSequence.isNotNullOrEmpty(): Boolean {
+    return !isNullOrEmpty()
+}
